cb8ed36a79262e7340bb064503f244189b6563df
yaphet17 <yafetberhanu3@gmail.com>
2022-06-22 07:49:38 +0300

    initial commit

---
+++ src/main/java/com/yaphet/hdfsfileviewer/HdfsFileViewerApplication.java
@@ -0,0 +1,13 @@
+package com.yaphet.hdfsfileviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdfsFileViewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdfsFileViewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yaphet/hdfsfileviewer/HdfsFileViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaphet.hdfsfileviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdfsFileViewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
